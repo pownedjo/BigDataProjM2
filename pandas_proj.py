fffbf492566a40b5dc6b91168f73cfe0eeb57494
jordant <jordan.tete@efrei.net>
2015-12-18 23:40:36 +0100

    Update pandas - visualisation dataset

--- pandas_proj.py
+++ pandas_proj.py
@@ -6,16 +6,13 @@ from sklearn.decomposition import PCA
 
 columns = ['Cultivar', 'Alcohol', 'Malic Acid', 'Ash', 'Alkalinity of Ash', 'Magnesium', 'Total phenol', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
 
-raw_dataset = pd.read_csv(open('wine.txt'), names=columns)
-description_of_dataset = raw_dataset.describe()
-print description_of_dataset
-
+columns_values = ['Alcohol', 'Malic Acid', 'Ash', 'Alkalinity of Ash', 'Magnesium', 'Total phenol', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
 
 
 ## Datas Visualisation - Display a List of Lists
-def visualize_datas(dataset):
+def visualize_datas(dataset, title):
     fig = plt.figure()
-    fig.suptitle('Datas Visualizations with Matplotlib', fontsize=18)
+    fig.suptitle(title, fontsize=18)
     plt.xlabel('Abscisse', fontsize=14)
     plt.ylabel('Ordonee', fontsize=14)
 
@@ -25,4 +22,42 @@ def visualize_datas(dataset):
     plt.show()  ## show graph
 
 
-## MAIN
+## SPLITING VALUES FOR EACH CULTIVATORS
+def split_datas_into_cultivators(dataset):
+	cultivar1 = dataset.loc[dataset['Cultivar'] == 1]
+	cultivar2 = dataset.loc[dataset['Cultivar'] == 2]
+	cultivar3 = dataset.loc[dataset['Cultivar'] == 3]
+	
+	display_dataset('Raw Dataset Visualisation', cultivar1, cultivar2, cultivar3)
+	
+	
+def display_dataset(title, dataset1, dataset2, dataset3):
+	fig = plt.figure()
+	fig.suptitle(title, fontsize=18)
+	
+	for name in columns_values:
+		plt.plot(dataset1[name], 'r')
+		plt.plot(dataset2[name], 'g')
+		plt.plot(dataset3[name], 'b')
+	
+	#for r in columns_values:
+	#	ax = plt.subplot(cultivar1[r], cultivar2[r], cultivar3[r])
+	#	fig.add_subplot(ax)
+	
+	plt.show()
+		
+	#s = cultivar1['Alcohol']
+	#print s
+	
+	
+
+### MAIN ###
+raw_dataset = pd.read_csv(open('wine.txt'), names=columns)
+
+#description_of_dataset = raw_dataset.describe()
+#alcohol_values = raw_dataset['Alcohol']
+
+split_datas_into_cultivators(raw_dataset)
+
+
+#visualize_datas(, 'Dataset Visualisation')
